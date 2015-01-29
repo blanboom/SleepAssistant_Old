5ed4ccc6420575d11fcfdc761a5f5c0804612f3c
Blanboom <i@blanboom.org>
2015-01-29 11:33:45 +0800

    [添加功能] 添加 RTC 驱动

---
+++ HardwareDrivers/RTC_Time/RTC_Time.h
@@ -0,0 +1,19 @@
+/*
+ * RTC_Time.h
+ *
+ *  Created on: 2014年11月16日
+ *      Author: maxz
+ */
+#ifndef _RTC_TIME_H_
+#define _RTC_TIME_H_
+
+#include <Time.h>
+
+void RTCTime_Init(void);
+void RTCTime_SetUnixTime(time_t time);
+void RTCTime_SetCalenderTime(struct tm t);
+time_t RTCTime_ConvCalendarToUnix(struct tm t);
+struct tm RTCTime_ConvUnixToCalendar(time_t t);
+extern time_t time(time_t* timer);
+
+#endif
