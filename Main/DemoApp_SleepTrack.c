9e4aa09b775463d8e49b0eb8b3746683ae4fcd5b
Blanboom <i@blanboom.org>
2015-05-09 21:59:14 +0800

    睡眠追踪程序更新

--- Main/DemoApp_SleepTrack.c
+++ Main/DemoApp_SleepTrack.c
@@ -11,15 +11,15 @@
 uint16_t sleepBadTotal = 0;
 uint16_t sleepTotal    = 0;
 
-void showNoteScreen(void);
-void showMainScreen(void);
+void showNoteScreen_SleepTrack(void);
+void showMainScreen_SleepTrack(void);
 void sleepTrack(void);
 
 void DemoApp_SleepTrack(void) {
-	showNoteScreen();
+	showNoteScreen_SleepTrack();
 	delay(200);
 	while (GUI_Touch_Read_2046() == 0);
-	showMainScreen();
+	showMainScreen_SleepTrack();
 	delay(500);
 	for(;;) {
 		sleepTrack();
@@ -27,12 +27,12 @@ void DemoApp_SleepTrack(void) {
 	}
 }
 
-void showNoteScreen(void) {
+void showNoteScreen_SleepTrack(void) {
 	ili9320_Clear(White);
 	GUI_DisplayBMP(0, 0, "/GUI/f1/nf1.bmp");
 }
 
-void showMainScreen(void) {
+void showMainScreen_SleepTrack(void) {
 	ili9320_Clear(White);
 	GUI_DisplayBMP(0, 0, "/GUI/f1/f1.bmp");
 	GUI_Main_DrawGraph_Prepare(0, 45, 220, 180, White);
