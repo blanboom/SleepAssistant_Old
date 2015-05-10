cca92449453189b3a92a313c0978caa863e25e4e
Blanboom <i@blanboom.org>
2015-05-10 15:46:31 +0800

    修改 DHT 11 驱动程序的引脚定义

--- HardwareDrivers/DHT11/dht11.h
+++ HardwareDrivers/DHT11/dht11.h
@@ -5,20 +5,16 @@
 #define __DHT11_H
 
 /* Includes ------------------------------------------------------------------*/
-#include "utils.h"
-#include "pin.h"
-#include "tim.h"
+#include "stm32f10x_conf.h"
 
 /* Exported constants --------------------------------------------------------*/
 #define MAX_TICS 10000
 #define DHT11_OK 0
 #define DHT11_NO_CONN 1
 #define DHT11_CS_ERROR 2
-#define DHT11_PORT GPIOA
-#define DHT11_PIN GPIO_Pin_5
+#define DHT11_PORT GPIOE
+#define DHT11_PIN GPIO_Pin_2
 
-//#define DHT11_PORT GPIOC
-//#define DHT11_PIN GPIO_Pin_2
 
 /* Exported macro ------------------------------------------------------------*/
 
@@ -28,6 +24,6 @@ float DHT22_Humidity(uint8_t *buf);
 float DHT22_Temperature(uint8_t *buf);
 uint8_t DHT11_Humidity(uint8_t *buf);
 uint8_t DHT11_Temperature(uint8_t *buf);
-uint8_t DHT11_pwm_Read(uint8_t *buf, uint32_t *dt, uint32_t *cnt);
+//uint8_t DHT11_pwm_Read(uint8_t *buf, uint32_t *dt, uint32_t *cnt);
 
 #endif /* __DHT11_H */
